d8c8d2f59efc753023e5f3fd0fc7e61c23b41f3a
Rémy BRUYERE <rbruyere.externe@bedrockstreaming.com>
2021-12-20 13:28:33 +0100

    chore: add tests

---
+++ tests/Request/AbtractRequestTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Scraper\ScraperAllocine\Tests\Request;
+
+use PHPUnit\Framework\TestCase;
+use Scraper\Scraper\Client;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
+abstract class AbtractRequestTest extends TestCase
+{
+    protected function getClient(string $fixture): Client
+    {
+        $responseInterface = $this->createMock(ResponseInterface::class);
+        $responseInterface
+            ->method('getStatusCode')->willReturn(200);
+        $responseInterface
+            ->method('getContent')->willReturn(file_get_contents(__DIR__ . '/../Fixtures/' . $fixture))
+        ;
+        $httpClient = $this->createMock(HttpClientInterface::class);
+        $httpClient
+            ->method('request')->willReturn($responseInterface)
+        ;
+        return new Client($httpClient);
+    }
+}
